3c820e989b0e55c92e190dfb782e31d0ffe59ddc
Yacov Uziel <yacovuziel@odeds-MacBook-Pro-2.local>
2018-10-22 16:09:08 +0300

    First Commit

---
+++ gongForgotPasswordDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gongForgotPasswordDemo
+//
+//  Created by Yacov Uziel on 22/10/2018.
+//  Copyright © 2018 Yacov Uziel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
